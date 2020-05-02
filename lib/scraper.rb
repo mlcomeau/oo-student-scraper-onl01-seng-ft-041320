ac55fdcaf681340ad0fea7ac41a3b47c734b1e99
Megan Comeau <mlcomeau99@gmail.com>
2020-05-02 19:57:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,8 @@ class Scraper
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
-      student_details[:profile_url] = student.css()
+      student_details[:profile_url] = student.css("a").attribute("href").value
+      students << student_details 
 
   end
 
